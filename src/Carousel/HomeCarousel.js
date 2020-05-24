edd81bee47b3f2d7ccb22c326c9a35b677b2e073
SadheeraSMahanama <sadheeramahanama94@gmail.com>
2020-05-24 19:49:10 +0530

    after mapping

--- src/Carousel/HomeCarousel.js
+++ src/Carousel/HomeCarousel.js
@@ -1,57 +1,49 @@
 import React, {useState} from "react"; 
 import Carousel from 'react-bootstrap/Carousel';
-import image1 from "./images/image1.jpg";
-import image2 from "./images/image2.jpg";
-import image3 from "./images/image3.jpg";
-// import image4 from "./images/image1.jpg";
-// import image5 from "./images/image1.jpg";
+
+const data = [
+  {
+   image: require('./images/image1.jpg'), 
+   caption:"Caption",
+   description:"Description Here"
+  },
+  {
+    image:require('./images/image2.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   },
+   {
+    image:require('./images/image3.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   } 
+]
+
 
 function HomeCarousel() {
   const [index, setIndex] = useState(0);
-
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
+       {data.map((slide, i) => {
+        return (
+          <Carousel.Item>        
         <img
           className="d-block w-100"
-          src={image1}
+          src={slide.image}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>{slide.caption}</h3>
+          <p>{slide.description}</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={image2}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={image3}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+        )
+      })}
+      
     </Carousel>
   );
 }
